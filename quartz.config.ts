165ccec8e42bc69e7e6112b849da9441f9368c10
Anthony Gasbarro <agasbarro36@gmail.com>
2024-11-14 17:20:15 -1000

    Quartz sync: Nov 14, 2024, 5:20 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🌊🐻🪴AwkwaBear Digital Garden ",
+    pageTitle: "🌊🐻🪴AwkwaBear Digital Pile",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
